21bea510925e14d8ac53e6242f6fe53ebe3127c0
Keldybek Berdaliev <33643909+berdaliev@users.noreply.github.com>
2018-04-11 23:11:01 -0500

    automated feature files

--- src/test/java/page/WordpressDashboardPage.java
+++ src/test/java/page/WordpressDashboardPage.java
@@ -1,5 +1,40 @@
 package page;
 
-public class WordpressDashboardPage {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.Driver;
+
+public class WordpressDashboardPage {
+	
+private WebDriver driver;
+	
+	public WordpressDashboardPage() {
+		driver=Driver.getDriver();
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath="(//*[text()='Dashboard'])")
+	public WebElement dashboard;
+	
+	@FindBy(xpath="(//*[text()='WordPress Gutenberg Demo and Talk'])")
+	public WebElement city;
+	
+	@FindBy (xpath="//section[@class='section text--small']")
+	public WebElement eventInfo;
+	
+	@FindBy (xpath="(//h4[.='Meetup'])")
+	public WebElement meetup;
+	
+	@FindBy (xpath="(//span[.='Groups'])")
+	public WebElement groups;
+	
+	@FindBy (css=".community-events-footer a")
+	public List<WebElement> worldCamps;
 }
+
---
+++ src/test/java/stepDefinitions/Dashboard_WorldCamps_News_Step_Def.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebElement;
+import org.testng.asserts.SoftAssert;
+
+import cucumber.api.java.en.Then;
+import page.WordpressDashboardPage;
+
+public class Dashboard_WorldCamps_News_Step_Def {
+	
+	@Then("^subscriber should be able to see upcoming WorldCamps$")
+	public void subscriber_should_be_able_to_see_upcoming_WorldCamps() {
+		WordpressDashboardPage dashboard = new WordpressDashboardPage();
+	    SoftAssert soft  = new SoftAssert();
+	    for(WebElement elem : dashboard.worldCamps){
+	    	soft.assertTrue(elem.isDisplayed(), "WorldCamp verification failed");
+	    	
+	    }
+	    soft.assertAll();
+	}
+
+	@Then("^subscriber should be able to see news$")
+	public void subscriber_should_be_able_to_see_news() {
+		subscriber_should_be_able_to_see_upcoming_WorldCamps();
+	}
+
+}
