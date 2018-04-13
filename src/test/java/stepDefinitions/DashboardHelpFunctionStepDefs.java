421e393789cbd95de0604f67a08b7076721fa4c7
arzuvbaymiyeva <arzuvbaymiyeva@10.0.0.158>
2018-04-13 13:00:37 -0500

    new code added

--- src/test/java/stepDefinitions/DashboardHelpFunctionStepDefs.java
+++ src/test/java/stepDefinitions/DashboardHelpFunctionStepDefs.java
@@ -2,10 +2,15 @@ package stepDefinitions;
 
 import static org.testng.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import page.WordpressDBHelpFunct;
@@ -17,6 +22,7 @@ public class DashboardHelpFunctionStepDefs {
 	WordpressDBHelpFunct DashboardFunctionality = new WordpressDBHelpFunct();
 	
 	WebDriver driver=Driver.getDriver();
+	
 	
 	@When("^subscriber clicks on Dashboard$")
 	public void subscriber_clicks_on_Dashboard() {
@@ -31,18 +37,22 @@ public class DashboardHelpFunctionStepDefs {
 
 	@When("^subscriber click on Help functionality$")
 	public void subscriber_click_on_Help_functionality() {
-	    // Write code here that turns the phrase above into concrete actions
+	   DashboardFunctionality.helpButton.click();
 	   
 	}
 
 	@Then("^the following options should be displayed in Help:$")
-	public void the_following_options_should_be_displayed_in_Help(DataTable arg1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc).
-	    // Field names for YourType must match the column names in 
-	    // your feature file (except for spaces and capitalization).
+	public void the_following_options_should_be_displayed_in_Help(List <String> helpOptions) {
+		
+	  String[]expectedArr =ConfigurationReader.getProperty("helpOptions").split(",");
+	  List<String>expectedList= Arrays.asList(expectedArr);
+	  
+	  System.out.println(expectedList);
+	  
+//	 List<WebElement> helpFuncOptions= getOptions();
+//	  System.out.println(helpFuncOptions);
+//	  Assert.assertEquals(expectedList, BrowserUtils.getElementsText(helpFuncOptions), "help func options not displayed");
+	   
 
 	}
 
