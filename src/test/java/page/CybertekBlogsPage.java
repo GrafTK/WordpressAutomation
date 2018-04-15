6f7ab5d987ec935870b3c8062e6c9dfce544bc85
GrafTK <33643939+GrafTK@users.noreply.github.com>
2018-04-14 19:31:02 -0500

    Update CybertekBlogsPage.java

--- src/test/java/page/CybertekBlogsPage.java
+++ src/test/java/page/CybertekBlogsPage.java
@@ -13,14 +13,7 @@ public CybertekBlogsPage() {
 	this.driver=Driver.getDriver();
 	PageFactory.initElements(driver, this);
 }
-//public void clickCybertekBlog() {
-//	Actions act=new Actions(driver);
-//	act.moveToElement(driver.findElement(By.xpath("//li[@id='wp-admin-bar-site-name']/a"))).build().perform();;
-//	driver.findElement(By.xpath("//a[.='Visit Site']")).click();
-//	act.moveToElement(driver.findElement(By.xpath("//li[@id='wp-admin-bar-site-name']/a"))).build().perform();;
-//	BrowserUtils.waitFor(3);
-//	driver.findElement(By.xpath("//a[.='Dashboard']")).click();
-//}
+
 @FindBy(xpath="(//li[@id='wp-admin-bar-site-name']//a)[1]")
 public WebElement cybertekBl;
 
