ca2a84f833200ec0f24c4b1386222ba34f3ba893
Tima <Tima@DESKTOP-BNBHHR8>
2018-04-16 14:28:14 -0500

    I hope final change

--- src/test/java/runners/SmokeTestRunner.java
+++ src/test/java/runners/SmokeTestRunner.java
@@ -4,7 +4,9 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-		plugin= {"pretty" ,"html:target/cucumber" ,"json:target/json"} ,
+		plugin= {"pretty", 
+				"html:target/cucumber-report",
+				"json:target/cucumber.json"} ,
 		features="src/test/resources/features" ,
 		glue="stepDefinitions" ,
 		tags="@smoketest" ,
